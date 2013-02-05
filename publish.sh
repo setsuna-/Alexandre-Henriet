aed6d05a2b72292b79d7126b898074a62e185a14
Alexandre Henriet <alex.henriet@gmail.com>
2013-02-05 09:19:07 +0100

    fixed: symlink turned into hardlink

--- publish.sh
+++ publish.sh
@@ -1,1 +1,15 @@
-/home/alex/bin/publish.sh
+#!/usr/bin/env bash
+
+# Config
+OUTPUT=/home/alex/www/mojave/output
+ARCHIVE=/tmp/site.tgz
+
+# Script
+DIR=`pwd`
+cd $OUTPUT
+rm -rf archives.html categories.html tags.html pages/cv.html feeds/
+tar -czf $ARCHIVE *
+echo -n 'user: ' && read USER
+scp $ARCHIVE $USER@vks:/home/$USER/www/alex.henriet.eu/
+ssh $USER@vks "cd /home/$USER/www/alex.henriet.eu && tar -zxf site.tgz && rm site.tgz"
+cd $DIR
